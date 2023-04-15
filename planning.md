93d652defbc3b710e4d68d41dbc45a00e3cd2797
Monica Gerard <mlgvla@gmail.com>
2023-04-15 00:32:39 -0400

    solutions

---
+++ planning.md
@@ -0,0 +1,14 @@
+Stage 1
+[x] get the form and attach an event listener to the form
+[x] get the task from the input
+[x] display the datah
+
+Stage 2
+[x] add delete button to each item on the list
+
+Stage 3
+[x] select a priority level for task before submitting
+[x] change color of task according to the priority level
+
+Stage 4
+[] allow the user to sort the priorities in both directions
---
+++ priority_level_deliverable/index.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+   addingEventListeners()
+})
+
+// get the form and add an event listener
+function addingEventListeners() {
+   document
+      .getElementById("create-task-form")
+      .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+    e.preventDefault()
+    console.log(e)
+    const task = e.target[0].value
+    const priorityLevel = parseInt(e.target.priority.value)
+    
+    displayTask(task, priorityLevel)
+}
+
+function displayTask(task, priorityLevel) {
+    const taskUl = document.getElementById("tasks")
+    const taskLi = document.createElement("li")
+    const deleteBtn = document.createElement("button")
+
+    deleteBtn.textContent = "x"
+    deleteBtn.addEventListener("click", deleteTask)
+
+    taskLi.textContent = task + " "
+    taskLi.style.color = getPriorityColor(priorityLevel)
+    taskLi.appendChild(deleteBtn)
+    taskUl.appendChild(taskLi)
+}
+
+function deleteTask(e) {
+    console.log(e)
+    e.target.parentNode.remove()
+}
+
+function getPriorityColor(priorityLevel) {
+    if (priorityLevel === 1) {
+        return "red"
+    } else if (priorityLevel === 2) {
+        return "blue"
+    } else {
+        return "green"
+    }
+}
